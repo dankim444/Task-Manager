59f334b208da927bcb045a3a89af344f53921967
dankim444 <dankim1@sas.upenn.edu>
2024-02-08 19:22:25 -0500

    Initial Commit

---
+++ Task Manager/Task_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagerApp.swift
+//  Task Manager
+//
+//  Created by Daniel Kim on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
